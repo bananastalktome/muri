f6592b368271a41d8434a24b7610b8dea1255a1e
Ubuntu Disk <billy@ubuntu.(none)>
2010-02-16 15:58:35 -0800

    changes

--- lib/auto_media_info/base.rb
+++ lib/auto_media_info/base.rb
@@ -1,21 +1,22 @@
 require 'uri'
 module AMI
   class NoTransformer < StandardError; end
+  class Parser
+    PARSERS = { }
   
-  PARSERS = { }
+    require "#{File.dirname(__FILE__) + '/filters/youtube.rb'}"
   
-  require "#{File.dirname(__FILE__) + '/filters/youtube.rb'}"
-  
-  def self.parse(url)
-    url_components = URI.parse(url)
-    if url_components.scheme.empty?
-      url = "http://#{url}"
+    def self.parse(url)
       url_components = URI.parse(url)
-    end
-    if parse_function = PARSERS[url_components.host]
-      self.send(parse_function, url)
-    else
-      raise NoTransformer.new("No Transformer found for URL")
+      if url_components.scheme.empty?
+        url = "http://#{url}"
+        url_components = URI.parse(url)
+      end
+      if parse_function = PARSERS[url_components.host]
+        self.send(parse_function, url)
+      else
+        raise NoTransformer.new("No Transformer found for URL")
+      end
     end
   end
 
--- lib/auto_media_info/filters/youtube.rb
+++ lib/auto_media_info/filters/youtube.rb
@@ -10,9 +10,9 @@ module Youtube
       end                            
     end
   end
-  def self.youtube_parse(url)
-    url
-  end
+#  def self.youtube_parse(url)
+#    url
+#  end
 #http://www.youtube.com/v/4CYDFoEz8rg&hl=en_US&fs=1&
 #http://www.youtube.com/watch?v=l983Uob0Seo&feature=rec-LGOUT-exp_fresh+div-1r-1-HM
 end
