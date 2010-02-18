d971952d400b6e44d45aa81ff988b63aafdcdf0c
Ubuntu Disk <billy@ubuntu.(none)>
2010-02-17 20:22:28 -0800

    Finally a good start

--- lib/auto_media_info/base.rb
+++ lib/auto_media_info/base.rb
@@ -1,18 +1,23 @@
 require 'uri'
 module AMI
   class NoTransformer < StandardError; end
-  class Parser
+
+  module Parser
+    class << self; attr_accessor :service, :url end
+    @service = ''
+    @url = ''
     PARSERS = { }
-    
-    require "#{File.dirname(__FILE__) + '/filters/youtube.rb'}"
+
+    include Filter::Youtube
     
     def self.parse(url)
       url_components = URI.parse(url)
-      if url_components.scheme.empty?
+      if url_components.scheme.nil?
         url = "http://#{url}"
         url_components = URI.parse(url)
       end
       if parse_function = PARSERS[url_components.host]
+        self.url = url        
         self.send(parse_function, url)
       else
         raise NoTransformer.new("No Transformer found for URL")
--- lib/auto_media_info/filters/youtube.rb
+++ lib/auto_media_info/filters/youtube.rb
@@ -1,18 +1,22 @@
-module Youtube
-  def self.included(base)
-    base.class_eval do
-      self::PARSERS["www.youtube.com"] = "youtube_parse"
-      self::PARSERS["youtube.com"] = "youtube_parse" 
-      
-      def self.youtube_parse(url)
-        url_components = URI.parse(url)
-        url_components
-      end                            
+module AMI
+  module Filter
+    module Youtube
+      def self.included(base)
+        base.class_eval do
+          self::PARSERS["www.youtube.com"] = "youtube_parse"
+          self::PARSERS["youtube.com"] = "youtube_parse"
+
+          def self.youtube_parse(url)
+            url_components = URI.parse(url)
+            self.service = 'Youtube'
+          end
+        end
+      end
     end
   end
+end
 #  def self.youtube_parse(url)
 #    url
 #  end
 #http://www.youtube.com/v/4CYDFoEz8rg&hl=en_US&fs=1&
 #http://www.youtube.com/watch?v=l983Uob0Seo&feature=rec-LGOUT-exp_fresh+div-1r-1-HM
-end
