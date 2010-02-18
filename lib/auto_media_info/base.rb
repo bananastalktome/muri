a4b65ac9fc26fe551ecc1390002574fa40ab4e8c
William Schneider <bananastalktome@gmail.com>
2010-02-18 14:18:06 -0500

    Revamped

--- lib/auto_media_info/base.rb
+++ lib/auto_media_info/base.rb
@@ -1,28 +1,61 @@
 require 'uri'
-module AMI
+class AMI
   class NoTransformer < StandardError; end
-
-  module Parser
-    class << self; attr_accessor :service, :url end
-    @service = ''
-    @url = ''
-    PARSERS = { }
-
-    include Filter::Youtube
     
-    def self.parse(url)
-      url_components = URI.parse(url)
-      if url_components.scheme.nil?
-        url = "http://#{url}"
-        url_components = URI.parse(url)
-      end
-      if parse_function = PARSERS[url_components.host]
-        self.url = url        
-        self.send(parse_function, url)
-      else
-        raise NoTransformer.new("No Transformer found for URL")
-      end
+  PARSERS = { }
+
+  include Youtube
+    
+  def self.parse(url)
+    self.new(url)
+  end
+    
+  def initialize(url)
+    @info = { }
+    _parse(url)
+  end  
+
+  def to_yaml
+    @info.to_yaml
+  end
+  
+  def to_s
+    @info.to_s
+  end
+  
+  def parsers
+    PARSERS.keys  
+  end
+  
+  private
+  
+  def _parse(raw_url)
+    @url = URI.parse(raw_url)
+    if @url.scheme.nil?
+      raw_url = "http://#{raw_url}"
+      @url = URI.parse(raw_url)
     end
+    if parse_function = PARSERS[@url.host]
+      @info[:url] = raw_url
+      send(parse_function)
+    else
+      raise NoTransformer.new("No Transformer found for URL")
+    end
+    
+  rescue => e
+    raise 'failed #{e.to_yaml}'
+  end
+      
+  def field_value(field_name)
+    if @info[field_name.to_sym] != nil
+      @info[field_name.to_sym]
+    else
+      nil
+    end
+  end
+  
+  def method_missing(func, args = nil)
+    field_value(func)
   end
 
 end
--- lib/auto_media_info/filters/youtube.rb
+++ lib/auto_media_info/filters/youtube.rb
@@ -1,19 +1,30 @@
-module AMI
-  module Filter
-    module Youtube
-      def self.included(base)
-        base.class_eval do
-          self::PARSERS["www.youtube.com"] = "youtube_parse"
-          self::PARSERS["youtube.com"] = "youtube_parse"
+require 'cgi'
+require 'pp'
+module Youtube
 
-          def self.youtube_parse(url)
-            url_components = URI.parse(url)
-            self.service = 'Youtube'
-          end
-        end
-      end
+  def self.included(base)
+    base.class_eval do
+      self::PARSERS["www.youtube.com"] = "youtube_parse"
+      self::PARSERS["youtube.com"] = "youtube_parse"
     end
   end
+  def youtube_parse
+    
+    @info[:service] = 'Youtube'
+    
+    if (@url.path == "/watch") && !@url.query.nil?
+      #params = url_components.query.to_params
+      params = CGI::parse(@url.query)
+      @info[:media_id] = params["v"].first
+      
+    elsif (@url.path =~ /\/v\/([a-zA-Z0-9\-\_]*)/i)
+      @info[:media_id] = $1
+      
+    end
+    pp @info
+    self
+  end
+  
 end
 #  def self.youtube_parse(url)
 #    url
