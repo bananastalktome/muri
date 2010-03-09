876e249b995a624021f3e7dac975270df4c1bc4d
William Schneider <bananastalktome@gmail.com>
2010-03-09 13:39:43 -0500

    Error catching for bad URI's

--- lib/muri/base.rb
+++ lib/muri/base.rb
@@ -65,7 +65,7 @@ class Muri
       else
         raise NoParser
       end
-    rescue NoParser, UnsupportedURI, BadURIError, InvalidURIError => e
+    rescue NoParser, UnsupportedURI, URI::BadURIError, URI::InvalidURIError => e
       @info[:errors] = "#{e}"
     end
   end
